341586c5d02714d18a8b59c8cc2f24cefc96ee90
Patryk <0989317@hr.nl>
2022-05-08 13:53:36 +0200

    Begin
    fix

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-var then = function (first, second) {
-    return fun(function (input) { return second(first(input)); });
-};
-var fun = function (f) {
-    var fDecorated = f;
+"use strict";
+const then = (first, second) => fun((input) => second(first(input)));
+const fun = (f) => {
+    const fDecorated = f;
     fDecorated.then = function (postProcess) {
         return then(this, postProcess);
     };
@@ -17,10 +16,10 @@ INVALID QUERY (No Address in Grades)
 students.Select("Name", "Surname").Include("Grades", q => q.Select("Grade", "Address" ))
 */
 //DATA TO TEST WITH
-var students = [
+let students = [
     { "Name": "John", "Surname": "Doe", "Grades": [{ "Grade": 10, "CourseId": 20 }] },
     { "Name": "Ellie", "Surname": "Bellie", "Grades": [{ "Grade": 5, "CourseId": 20 }] },
     { "Name": "Albert", "Surname": "Einstein", "Grades": [{ "Grade": 7, "CourseId": 20 }] },
     { "Name": "Patryk", "Surname": "Kulik", "Grades": [{ "Grade": 9, "CourseId": 20 }] }
 ];
-console.log(students.map(function (t) { return [t.Name, t.Surname, t.Grades[0].CourseId]; }));
+console.log(students.map(t => [t.Name, t.Surname, t.Grades[0].CourseId]));
